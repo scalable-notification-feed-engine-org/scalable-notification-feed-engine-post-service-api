fa3c761a4d5afd58f1f4997415e12f2b8aa9682a
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-20 17:04:36 +0530

    feat(post): register PrismaModule in PostModule

---
+++ src/post/post.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { CreateCommentDto } from './dto/create-comment.dto';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.create(createPostDto);
+  }
+  // @Query('tenantId') tenantId: string,
+  @Get()
+  findAll(@Query('userId') userId: string) {
+    return this.postService.findAll(userId);
+  }
+
+  @Post(':id/like')
+  async togglePostLike(
+    @Param('id') postId: string,
+    @Body('userId') userId: string,
+  ) {
+    return await this.postService.toggleLike(postId, userId);
+  }
+
+  @Post(':id/comment')
+  async addComment(
+    @Param('id') postId: string,
+    @Body() createCommentDto: CreateCommentDto,
+  ) {
+
+    console.log("addComment",  createCommentDto.userId);
+
+    return await this.postService.addComment(
+      postId,
+      createCommentDto.userId,
+      createCommentDto.content,
+    );
+  }
+}
