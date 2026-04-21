d8df4c74f6283d8074fa253e14d94c68f8dec868
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-21 15:49:32 +0530

    feat(post): register Kafka client in PostModule

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'POST_KAFKA_CLIENT',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'post-producer-group',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
